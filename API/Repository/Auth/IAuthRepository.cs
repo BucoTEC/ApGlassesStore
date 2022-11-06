d528e3165d40e3691ba559d3728886fef3722d2a
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-11-06 17:20:41 +0100

    update auth interface

--- API/Repository/Auth/IAuthRepository.cs
+++ API/Repository/Auth/IAuthRepository.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Data.Models.Auth;
+using Microsoft.AspNetCore.Identity;
 
 namespace API.Repository.Auth
 {
     public interface IAuthRepository
     {
-        
+        Task<IdentityResult> Signup(SignupModel data);
+
+        Task<string> LoginAsync(SignInModel signInModel);
     }
 }
