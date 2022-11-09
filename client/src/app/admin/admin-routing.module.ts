17c98ccc3f68262207d17d61129c618adb3ce5ef
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-11-09 23:31:58 +0100

    setup admin routing

--- client/src/app/admin/admin-routing.module.ts
+++ client/src/app/admin/admin-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { CreateEditProductComponent } from './create-edit-product/create-edit-product.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AdminDashboardComponent },
+  { path: 'products', component: ProductListComponent },
+  { path: 'products/create', component: CreateEditProductComponent },
+  { path: 'products/:id', component: CreateEditProductComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AdminRoutingModule { }
+export class AdminRoutingModule {}
--- client/src/app/admin/admin.module.ts
+++ client/src/app/admin/admin.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { AdminComponent } from './admin.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { CreateEditProductComponent } from './create-edit-product/create-edit-product.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AdminComponent,
+    ProductListComponent,
+    CreateEditProductComponent,
+    AdminDashboardComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule
