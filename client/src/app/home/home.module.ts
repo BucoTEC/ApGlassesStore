d765c7034a576862f2ab8b2f3f43d380c071c5e9
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-11-09 10:37:01 +0100

    setup basic conection to home component

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -2,16 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
-import { HomePageComponent } from './components/home-page/home-page.component';
-
+import { HomeComponent } from './home.component';
 
 @NgModule({
-  declarations: [
-    HomePageComponent
-  ],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  declarations: [HomeComponent],
+  imports: [CommonModule, HomeRoutingModule],
 })
-export class HomeModule { }
+export class HomeModule {}
