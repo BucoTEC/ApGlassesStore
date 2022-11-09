0adc4819e68e04544cffcd0582843f1dd29d2585
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-11-09 23:22:43 +0100

    setup user

---
+++ client/src/app/user/user-profile-layout/user-profile-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile-layout',
+  templateUrl: './user-profile-layout.component.html',
+  styleUrls: ['./user-profile-layout.component.scss']
+})
+export class UserProfileLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- client/src/app/user/user.module.ts
+++ client/src/app/user/user.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
-
+import { UserComponent } from './user.component';
+import { OrderListComponent } from './order-list/order-list.component';
+import { OrderReviewComponent } from './order-review/order-review.component';
+import { UserProfileLayoutComponent } from './user-profile-layout/user-profile-layout.component';
+import { UserSettingsComponent } from './user-settings/user-settings.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    UserRoutingModule
-  ]
+  declarations: [UserComponent, OrderListComponent, OrderReviewComponent, UserProfileLayoutComponent, UserSettingsComponent],
+  imports: [CommonModule, UserRoutingModule],
 })
-export class UserModule { }
+export class UserModule {}
