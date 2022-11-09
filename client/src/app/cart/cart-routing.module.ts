937429592470f882f8f4d3e06e4e84a9468280da
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-11-09 10:58:06 +0100

    conect checkout

--- client/src/app/cart/cart-routing.module.ts
+++ client/src/app/cart/cart-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartPageComponent } from './components/cart-page/cart-page.component';
+import { CheckoutPageComponent } from './components/checkout-page/checkout-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CartPageComponent,
+  },
+  {
+    path: 'checkout',
+    component: CheckoutPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
