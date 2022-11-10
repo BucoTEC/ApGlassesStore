137908eff6731594dedf45dc09473f794dd006fa
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-11-10 10:40:57 +0100

    add main hero banner

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
+import { MainHeroBannerComponent } from './main-hero-banner/main-hero-banner.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, MainHeroBannerComponent],
   imports: [CommonModule, SharedModule],
 })
 export class HomeModule {}
---
+++ client/src/app/home/main-hero-banner/main-hero-banner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-hero-banner',
+  templateUrl: './main-hero-banner.component.html',
+  styleUrls: ['./main-hero-banner.component.scss']
+})
+export class MainHeroBannerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
