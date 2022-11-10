f5f938446645b6912e8e15722458766902c46da8
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-11-10 11:04:06 +0100

    setup baisc product card

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
 import { MainHeroBannerComponent } from './main-hero-banner/main-hero-banner.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
-  declarations: [HomeComponent, MainHeroBannerComponent],
+  declarations: [HomeComponent, MainHeroBannerComponent, ProductListComponent],
   imports: [CommonModule, SharedModule],
 })
 export class HomeModule {}
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
 import { RouterModule } from '@angular/router';
 import { ProductSearchComponent } from './product-search/product-search.component';
+import { ProductCardComponent } from './product-card/product-card.component';
 
 @NgModule({
-  declarations: [MainLayoutComponent, ProductSearchComponent],
+  declarations: [
+    MainLayoutComponent,
+    ProductSearchComponent,
+    ProductCardComponent,
+  ],
   imports: [CommonModule, RouterModule],
-  exports: [MainLayoutComponent, ProductSearchComponent],
+  exports: [MainLayoutComponent, ProductSearchComponent, ProductCardComponent],
 })
 export class SharedModule {}
